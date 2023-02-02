6f4c1656709f35ce30112e4e9104430804dcde68
Gabriela Payano <gabipayano@Gabrielas-MacBook-Air.local>
2023-02-01 21:20:34 -0800

    jump jump

--- main.js
+++ main.js
@@ -1,36 +1,57 @@
-  document.addEventListener('DOMContentLoaded', ()=> {
+  document.addEventListener('DOMContentLoaded', () => {
       const dino = document.querySelector('.dino');
+      let isJumping = false;
+      let gravity = 0.9;
 
-      function control(e){
-          if(e.keyCode === 32){
-              jump()
+
+      function control(e) {
+          if (e.keyCode === 32) {
+              if (!isJumping) {
+                  isJumping = true;
+                  jump()
+              }
               //space
               console.log('space entered')
-
           }
       }
       document.addEventListener('keyup', control)
 
-      function jump(){
-          let position = 0;
-          let timerId = setInterval(function(){
+      let position = 0;
+
+      function jump() {
+          let count = 0;
+          let timerId = setInterval(function () {
               console.log('up');
               position += 30
               dino.style.bottom = position + 'px';
 
-
-              if(position === 150){
+              if (count === 15) { //divisible by 30
                   clearInterval(timerId);
                   console.log('down');
-                position -= 30;
-    
+                  let downTimerId = setInterval(function () {
+                      if (count === 0) {
+                          clearInterval(downTimerId)
+                          isJumping = false;
+
+                      }
+                      position -= 10;
+                      count--;
+                      position = position * gravity;
+                      dino.style.bottom = position + 'px';
+                  }, 20)
+
               }
+              count ++;
+              position += 30;
+              position = position * gravity;
+              dino.style.bottom = position + 'px';
+
 
           }, 20)
-          if(e.keyCode === 38){
-              //up arrow
+          //   if(e.keyCode === 38){
+          //       //up arrow
 
-          }
-          
+          //   }
+
       }
   })
